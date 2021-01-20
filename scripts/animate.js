5504fce93136281cbc00377d40df187097deea69
Mathew de Vin <mathewdevin03@gmail.com>
2021-01-20 00:24:17 -0800

    Website flow created with grid

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,15 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let timeline = new TimelineMax({paused: true});
+let timelineMain = new TimelineMax({paused: true}),
+    t1 = new TimelineMax();
 
-timeline.to(".loading__bg", {
+t1.to(".loading__bg", {
   width: "14rem",
   height: "14rem",
   borderRadius: "50%",
   duration: 2,
-  delay: 1
 });
 
+timelineMain.add([t1]);
+
 window.onload = () => {
-  timeline.play();
+  timelineMain.play();
 }
